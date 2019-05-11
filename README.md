446ef7b214079b8ddb56fe6dd3643d52d4d4e2e7
JoepKockelkorn <joepkockelkorn@gmail.com>
2019-05-11 19:35:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-q9oexs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-q9oexs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import dayjs from 'dayjs';
+import 'dayjs/locale/nl';
+import 'dayjs/locale/fr';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
+
+dayjs.extend(LocalizedFormat)
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  locales = ['en', 'nl', 'fr'];
+  formats = ['L', 'LLLL' , 'llll'];
+  currentLocale = 'en';
+  currentFormat = 'L';
+
+  today = dayjs();
+
+  get formatted() {
+    return this.today.locale(this.currentLocale).format(this.currentFormat);
+  }
+}
